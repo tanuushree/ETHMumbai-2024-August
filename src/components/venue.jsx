f0bfde26bd64e423230756af3c9b0a04b1b35748
Unmani Shinde <mailto.unmani@gmail.com>
2024-03-09 23:19:07 +0530

    fixed image compression (hopefully).

--- src/components/venue.jsx
+++ src/components/venue.jsx
@@ -5,17 +5,20 @@ import { FaMapMarkerAlt } from "react-icons/fa";
 
 function Venue() {
     return(<div style={{marginBottom:'10vh'}} className="venue-specifications text-color-purple">
-
-<img className="venue-image" src={FullVenue} alt="Location Image"></img>
+<p className="venue-heading">Venue</p>
+<img className="venue-image h-full w-full" src={FullVenue} alt="Location Image"></img>
 
 <div style={{paddingLeft:'2vw',paddingTop:'4vh'}}className="p-2 flex-col">
-        <p className="venue-heading">Venue</p>
+        
         <div className="venue-location flex flex-row">
-        <FaMapMarkerAlt style={{transform:'scale(1.25)',marginTop:'2vh',marginRight:"0.5vw"}}/> 
-        <p style={{paddingLeft:'0',fontSize:'large'}} className="flex-1 p-2 ml-1 text-sm font-Herokid text-color-black">Pioneer Hall, Bandra West</p>
+        
+        <p style={{paddingLeft:'0',fontSize:'x-large'}} className="flex-1 p-2 ml-1 text-sm font-Herokid text-color-black">📍Pioneer Hall, Bandra West</p>
         </div>
         
-        <a href='https://maps.app.goo.gl/ur44axfaYf3s8G6K7'style={{transform:'scale(0.75)'}} class="btn cta bg">Get Directions</a>
+        <a href='https://maps.app.goo.gl/ur44axfaYf3s8G6K7'style={{transform:'scale(0.75)'}} class="btn cta bg" 
+        target="_blank"  // This attribute opens the link in a new window/tab
+        rel="noopener noreferrer"  // Recommended for security to avoid potential vulnerabilities
+        >Get Directions</a>
             
         </div>
    
