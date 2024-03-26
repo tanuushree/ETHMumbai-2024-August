6fbb92719a02721609a50a35c92467191606256b
Tanu Shree <tanushree.wrk@gmail.com>
2024-03-27 04:58:17 +0530

    Update bounties.jsx

--- src/components/bounties.jsx
+++ src/components/bounties.jsx
@@ -9,7 +9,7 @@ import AnonAadharLogo from "../assets/bounties/Anon Aadhar - $3K.png";
 import TheGraphLogo from '../assets/bounties/The Graph - $2K.png';
 import VaraNetwork from '../assets/bounties/Vara - $2K.png';
 import Polygon from '../assets/bounties/Polygon - $2.5K.png';
-import Fuel from '../assets/bounties/Fuel - 4Kv2';
+import Fuel from '../assets/bounties/Fuel - 4Kv2.png';
 
 import BountiesLogo from "../assets/bounties.png";
 
